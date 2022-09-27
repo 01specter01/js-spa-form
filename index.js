00cb8facef4ea9fcb18ad6f534ee5a4a77659081
Leo Volwahsen <volwahsenl@gmail.com>
2022-09-27 09:20:07 +0200

    g

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const vorNameElem = document.querySelector("#vorName");
-
-if (!vorNameElem.includes()) {
-    console.log("test");
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
+    const input = document.querySelectorAll("input");
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        const vorNameCheck = form.elements.vorName.value;
+        if (vorNameCheck.includes("xxa")) {
+            console.log(true);
+        } else {
+            console.log(false);
+        }
+    });
+});
